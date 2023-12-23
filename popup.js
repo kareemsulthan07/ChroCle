29fb14632a0869663fa35b0b1a6a4f491e6f93bc
Kareem Sulthan <itsmystyle.shaik@gmail.com>
2023-12-23 19:45:51 +0530

    my activity link to popup

---
+++ popup.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function () {
+        document.getElementById('lastHourButton').addEventListener('click', onButtonClick);
+        document.getElementById('last24HoursButton').addEventListener('click', onButtonClick);
+        document.getElementById('last7DaysButton').addEventListener('click', onButtonClick);
+        document.getElementById('last4WeeksButton').addEventListener('click', onButtonClick);
+        document.getElementById('allTimeButton').addEventListener('click', onButtonClick);
+    });
+
+    function onButtonClick(event) {
+        const button = event.target;
+        const buttonId = button.id;
+
+        switch (buttonId) {
+            case 'lastHourButton':
+                {
+                    const endTime = new Date().getTime();
+                    const startTime = new Date().getTime() - 60 * 60 * 1000;
+                    clearHistory(endTime, startTime);
+                }
+                break;
+
+            case 'last24HoursButton':
+                {
+                    const endTime = new Date().getTime();
+                    const startTime = new Date().getTime() - 24 * 60 * 60 * 1000;
+                    clearHistory(endTime, startTime);
+                }
+                break;
+
+            case 'last7DaysButton':
+                {
+                    const endTime = new Date().getTime();
+                    const startTime = new Date().getTime() - 7 * 24 * 60 * 60 * 1000;
+                    clearHistory(endTime, startTime);
+                }
+                break;
+
+            case 'last4WeeksButton':
+                {
+                    const endTime = new Date().getTime();
+                    const startTime = new Date().getTime() - 28 * 24 * 60 * 60 * 1000;
+                    clearHistory(endTime, startTime);
+                }
+                break;
+
+            case 'allTimeButton':
+                {
+                    chrome.history.deleteAll(() => void {});
+                }
+                break;
+
+        }
+    }
+
+    function clearHistory(endTime, startTime) {
+        chrome.history.deleteRange({ endTime: endTime, startTime: startTime },
+            () => void {});
+    }
+
+});
